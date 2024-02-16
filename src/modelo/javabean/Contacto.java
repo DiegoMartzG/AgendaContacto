b382ab43df5a57550592fd49828f2a31ee8c4677
Diego <diegomartinezg382@gmail.com>
2024-02-16 16:58:05 +0100

    feat(contacto): creación de la clase Contacto con atrivutos privado y metodos

--- src/modelo/javabean/Contacto.java
+++ src/modelo/javabean/Contacto.java
@@ -1,2 +1,79 @@
-package modelo.javabean;public class Contacto {
+package modelo.javabean;
+
+import java.util.Objects;
+
+public class Contacto {
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String email;
+    private String empresa;
+
+    // constructor con todo
+    public Contacto(String nombre, String apellidos, String telefono, String email, String empresa) {
+        super();
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+        this.empresa = empresa;
+    }
+    // constructor sin nada
+    public Contacto() {
+        super();
+    }
+
+
+    //getter and setter
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmpresa() {
+        return empresa;
+    }
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+    //ToString
+    @Override
+    public String toString() {
+        return "Contacto [nombre=" + nombre + ", apellidos=" + apellidos + ", telefono=" + telefono + ", email=" + email
+                + ", empresa=" + empresa + "]";
+    }
+    // hashCode y Equals
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto = (Contacto) o;
+        return Objects.equals(nombre, contacto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
